0bd47786578116285f8f1a7c9deca02c5a024b65
vellanki2025 <vellanki2025@gmail.com>
2024-06-25 19:21:42 -0700

    Dependent libraries
    Dependent libraries

--- app/src/main/java/com/android/my/paintapp/MainActivity.kt
+++ app/src/main/java/com/android/my/paintapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.android.my.paintapp
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
